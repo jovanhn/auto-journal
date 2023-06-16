b641155b7a95906896e5c1081c64a1b6057b0f90
Jovan Suberic <suberic.jovan@gmail.com>
2023-06-16 08:56:44 +0200

    Initial commit

---
+++ src/main/java/com/subtech/autojournal/AutojournalApplication.java
@@ -0,0 +1,13 @@
+package com.subtech.autojournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutojournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutojournalApplication.class, args);
+	}
+
+}
